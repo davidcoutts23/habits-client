c2a31103d77899a8335ea753ae53c12131b29a96
davidcoutts23 <david.coutts@envato.com>
2022-02-14 21:15:03 +1100

    colour border based on habit rating

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -2,18 +2,58 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 
 export default function HabitComponent({ habit }) {
-  return (
-    <Card className="mt-2" style={{ width: "100%" }}>
-      <Card.Body>
-        <Card.Title>{habit.habitName}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{habit.habitRating}</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-  );
+  if ((habit.habitRating == "Positive")) {
+    return (
+      <Card className="mt-2" style={{ width: "100%" }} border="success">
+        <Card.Body>
+          <Card.Title>{habit.habitName}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {habit.habitRating}
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    );
+  }
+  if ((habit.habitRating == "Negative")) {
+    return (
+      <Card className="mt-2" style={{ width: "100%" }} border="danger">
+        <Card.Body>
+          <Card.Title>{habit.habitName}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {habit.habitRating}
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    );
+  }
+  if ((habit.habitRating == "Neutral")) {
+    return (
+      <Card className="mt-2" style={{ width: "100%" }} border="warning">
+        <Card.Body>
+          <Card.Title>{habit.habitName}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {habit.habitRating}
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
