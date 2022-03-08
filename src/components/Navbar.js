e2aa55cfe4da626c777d85205db303f2942d500e
davidcoutts23 <david.coutts@envato.com>
2022-03-09 10:21:11 +1100

    Change app name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function NavbarComponent() {
     <Router>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="home">Dave's habit app</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
