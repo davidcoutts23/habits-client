a54486d54c77d9b85e2616e0f7525d4405c96843
davidcoutts23 <david.coutts@envato.com>
2022-03-31 11:18:09 +1100

    Add add application intention component to existing habit

--- src/components/habit/Habit.js
+++ src/components/habit/Habit.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import ApplicationIntention from "../applicationIntention/ApplicationIntention";
+import AddApplicationIntention from "../applicationIntention/AddApplicationIntention";
 
 export default function Habit({ habit }) {
+  const handleAddApplicationIntention = (applicationIntention) => {
+    console.log(applicationIntention);
+  };
+
   return (
     <Card
       className="mt-2"
@@ -22,6 +27,9 @@ export default function Habit({ habit }) {
             />
           );
         })}
+        <AddApplicationIntention
+          handleAddApplicationIntention={handleAddApplicationIntention}
+        />
       </Card.Body>
     </Card>
   );
